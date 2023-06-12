a49690cdd607ff6a80fb3f85bb3e5a7834ca61d8
yuliu <yul325@pitt.edu>
2023-06-12 08:36:27 -0400

    6/12

---
+++ 228. Summary Range/solution.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution implements solutionInterface{
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        StringBuilder string = new StringBuilder();
+        int startNum = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(i == 0){
+                string.append("" + nums[0]);
+                startNum = nums[0];
+            }
+
+            if( i != 0 && (nums[i] - nums[i-1]) != 1 ){
+                if(startNum != nums[i - 1])
+                    string.append("->" + nums[i-1]);
+                result.add(string.toString());
+                string = new StringBuilder();
+                string.append("" + nums[i]);
+                startNum = nums[i];
+            }
+            if( i == nums.length - 1){
+                if(startNum != nums[i])
+                    string.append("->" + nums[i]);
+                result.add(string.toString());
+            }
+        }
+        return result;
+    }
+}
---
+++ 228. Summary Range/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String args[]){
+        solutionInterface solution = new Solution();
+        int[] nums = {0,1,2,4,5,7};
+        solution.summaryRanges(nums);
+    }
+}
