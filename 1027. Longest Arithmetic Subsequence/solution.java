473851f92f8a0c23dae4e9b762122012c1a3b825
yuliu <yul325@pitt.edu>
2023-06-23 07:54:32 -0400

    6/23 Unsolved!!!!!!!!!

---
+++ 1027. Longest Arithmetic Subsequence/solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution implements solutionInterface{
+    public int longestArithSeqLength(int[] nums) {
+        int result = 0;
+        HashMap<String, Integer> dp = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                int diff = Math.abs(nums[j] - nums[i]);
+                String left = i + "+" + diff;  //int[] left = {i, diff};
+                String right = j + "+" + diff;//int[] right = {j, diff};
+                if(dp.get(left) != null)
+                    dp.put(right, dp.get(left) + 1);
+                else 
+                    dp.put(right, 2);
+                result = Math.max(dp.get(right), result);
+            }
+        }
+        return result;
+    }
+}
---
+++ 1027. Longest Arithmetic Subsequence/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String args[]){
+        int[] nums = {3, 6, 9, 12};
+        solutionInterface solution = new Solution();
+        int a = solution.longestArithSeqLength(nums);
+    }
+}
